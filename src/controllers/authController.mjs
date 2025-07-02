e2d6a1462607efd750c7b8512701ee6fd63d9de9
Mulkiani <raisahsn19@gmail.com>
2025-07-02 19:56:21 +0700

    update

--- src/controllers/authController.mjs
+++ src/controllers/authController.mjs
@@ -1,75 +1,70 @@
 import authRepository from "../repository/authRepository.mjs";
-import dotenv from 'dotenv';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import dotenv from "dotenv";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { successResponse, errorResponse } from "../utils/apiResponseUtils.mjs";
 dotenv.config();
 
 const authController = {
-    async register(req,res){
-        try{
+  async register(req, res) {
+    try {
+      const { username, password, idRole, name } = req.body;
+      const hashPassword = await bcrypt.hash(password, 10);
 
-            const { username, password, idRole, name } = req.body;
-            const hashPassword = await bcrypt.hash(password, 10);
-        
-            const createUser = await authRepository.register(
-                username, 
-                hashPassword,
-                idRole, 
-                name,
-            );
+      const createUser = await authRepository.register(
+        username,
+        hashPassword,
+        idRole,
+        name
+      );
 
-            return successResponse(res, {
-                statusCode : 201, 
-                message : 'create user success', 
-                data : {
-                    user : createUser
-                }
-            })
-        }catch(error){
-            return errorResponse(res, {
-                statusCode : error.status || 500, 
-                message : error.message
-            } )
-        }
-    },
-
-    async login(req,res){
-        try{
-            
-            const { username, password } = req.body;
-
-            const user = await authRepository.login( 
-                username,
-                password,
-            ); 
-            
-            const token = jwt.sign(
-                {
-                    username : username,
-                    userId : user.idAccount
-                }, 
-                process.env.SECRET_KEY,
-                {
-                    expiresIn : '24h'
-                }
-            )
-
-            return successResponse(res, {
-                statusCode : 200, 
-                message  : 'login success',
-                data : {
-                    token : token, 
-                    user : user
-                }
-            })
-        }catch(error){
-            return errorResponse(res, {
-                statusCode : error.status || 500, 
-                message : error.message
-            } )
-        }
+      return successResponse(res, {
+        statusCode: 201,
+        message: "create user success",
+        data: {
+          user: createUser,
+        },
+      });
+    } catch (error) {
+      return errorResponse(res, {
+        statusCode: error.status || 500,
+        message: error.message,
+      });
     }
-}
+  },
+
+  async login(req, res) {
+    try {
+      const { username, password } = req.body;
+
+      const user = await authRepository.login(username, password);
+
+      const token = jwt.sign(
+        {
+          username: username,
+          userId: user.idAccount,
+        },
+        process.env.SECRET_KEY,
+        {
+          expiresIn: "24h",
+        }
+      );
+
+      return successResponse(res, {
+        statusCode: 200,
+        message: "login success",
+        data: {
+          token: token,
+          user: user,
+        },
+      });
+    } catch (error) {
+      return errorResponse(res, {
+        statusCode: error.status || 500,
+        message: error.message,
+      });
+    }
+  },
+};
 
 export default authController;
--- src/validations/authValidations.mjs
+++ src/validations/authValidations.mjs
@@ -3,16 +3,16 @@ import Joi from "joi";
 const username = Joi.string().min(3).max(30).required();
 const password = Joi.string().min(6).required();
 const idRole = Joi.number().integer().required();
-const name = Joi.string().required()
+const name = Joi.string().required();
 
 export const registerSchema = Joi.object({
   username,
   password,
   name,
-  idRole
+  idRole,
 });
 
 export const loginSchema = Joi.object({
   username,
-  password
+  password,
 });
